bfe912cbaec5703a33994eb84bf5c9fba208f658
liugs <lgspw2.10>
2020-07-09 14:15:46 +0800

    delete remark

--- src/main/java/ToolTest.java
+++ src/main/java/ToolTest.java
@@ -10,8 +10,6 @@ import java.util.Map;
  * @Description 工具测试类
  * @Author liugs
  * @Date 2020/7/9 9:38:22
- * @Copyright 2020 www.tydic.com Inc. All rights reserved.
- * 注意 本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class ToolTest {
 
--- src/main/java/com/liugs/tool/constants/ToolConstants.java
+++ src/main/java/com/liugs/tool/constants/ToolConstants.java
@@ -5,8 +5,6 @@ package com.liugs.tool.constants;
  * @Description 通用常量类
  * @Author liugs
  * @Date 2020/7/9 9:40:57
- * @Copyright 2020 www.tydic.com Inc. All rights reserved.
- * 注意 本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class ToolConstants {
 
