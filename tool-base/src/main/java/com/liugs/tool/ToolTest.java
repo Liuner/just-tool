362e5a0503284cda7c7272966bece0cb5b389853
liugs <lgspw2.10>
2021-03-02 17:31:00 +0800

    提交base

---
+++ tool-base/src/main/java/com/liugs/tool/ToolTest.java
@@ -0,0 +1,499 @@
+package com.liugs.tool;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.liugs.tool.base.Console;
+import com.liugs.tool.constant.ToolConstants;
+import com.liugs.tool.constant.ToolException;
+import com.liugs.tool.encode.EncodeExecuter;
+import com.liugs.tool.encode.RsaEncodeTool;
+import org.joda.time.DateTime;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @ClassName ToolTest
+ * @Description 工具测试类
+ * @Author liugs
+ * @Date 2020/7/9 9:38:22
+ */
+public class ToolTest {
+
+    public static void main(String[] args) {
+      /*  try {
+            enCode();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        /*dealNum();*/
+
+//        dealListSort();
+
+//        ToolExceptionTest();
+
+//        sort();
+
+        test();
+
+//        listToMap();
+
+//        listPage();
+    }
+
+
+
+    private static void test() {
+        /*String url = "http://172.16.100.36:18000/upload/cms/column/10001_banner/index.html";
+        String result = HttpUtil.get(url);
+        System.out.println("result:" + result);
+        JSONObject jsonObject = JSON.parseObject(result);
+
+        JSONArray rows = (JSONArray) jsonObject.get("rows");
+        System.out.println(rows);*/
+
+//        Set<String> keySet = ToolPropertiesKey.getKeySet();
+//        System.out.println(JSON.toJSONString(keySet));
+
+        /*String testStr = "key-value&name-liugs&age-24&";
+        String enCodeStr = null;
+        try {
+            enCodeStr = URLEncoder.encode(testStr, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String deCodeStr = null;
+        try {
+            deCodeStr = URLDecoder.decode(enCodeStr, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String[] strs = deCodeStr.split("&");
+        Map<String, String> map = new HashMap<>(16);
+        for (String s : strs) {
+            String[] str = s.split("-");
+            map.put(str[0], str[1]);
+        }
+
+        Console.show(JSON.toJSONString(map));*/
+
+//        String dateStr = "2020-09-15 16:17:22";
+//        DateTime tradeTime = DateTime.parse(dateStr, DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss"));
+//        Console.show(tradeTime.toString("yyyyHHdd"));
+//        Console.show(tradeTime.toString("HHmmss"));
+
+        /*Map<String, String> valueMap = new HashMap<>(16);
+        valueMap.put("Version", "20150922");
+        valueMap.put("MerId", "000092009169004");
+        valueMap.put("MerOrderNo", "dfsrwr23423423");
+        valueMap.put("TranDate", "20200919");
+        valueMap.put("TranTime", "175823");
+        valueMap.put("OrderAmt", "1");
+        valueMap.put("BusiType", "0001");
+        valueMap.put("MerBgUrl", "www.baidu.com");
+        valueMap.put("Signature", "sdfsdfsdfsdf");
+
+        String formHead = "<form name=\"message_form\" method=\"post\" action=\"URL\">";
+        String inputItem = "<input type=\"hidden\" name=\"CODE\" value=\"VALUE\">";
+        String submit = "<input type=\"submit\" value=\"立即支付\" style=\"display:none\" >";
+        String script = "<script>document.forms[0].submit();</script>";
+
+        StringBuilder htmlBuilder =new StringBuilder();
+        htmlBuilder.append(formHead.replace("URL", ""));
+        for (String key : valueMap.keySet()) {
+            String input = inputItem.replace("CODE", key).replace("VALUE", valueMap.get(key));
+            htmlBuilder.append(input);
+        }
+        htmlBuilder.append(submit);
+        htmlBuilder.append("</form>");
+        htmlBuilder.append(script);
+
+        System.out.println(htmlBuilder);*/
+
+        /*Console.show("时间戳：" + DateTime.now().getMillis());
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        long timestamp = cal.getTimeInMillis();
+        Console.show("时间戳：" + timestamp);*/
+
+//        String resultStr = HttpUtil.get("https://www.cnncmall.com/upload/cms/column/12323_banner/index.html");
+        /*JSONObject columnJson = new JSONObject();
+        columnJson.put("ceshi", "fdsfas");
+//        String result = HttpUtil.post("http://192.168.3.53:9000/ms-mcms/cms/service/addColumnService.do", columnJson.toJSONString());
+        String result = null;
+        JSONObject resultJson = JSON.parseObject(result);
+        Console.show(resultJson.toString());*/
+
+        /*String str = "{\n" +
+                "    \"goodsSubId\": \"323\",\n" +
+                "    \"goodsName\": \"商品名称\",\n" +
+                "    \"payeeCompanyName\": \"发射点发\",\n" +
+                "    \"goodsNumber\": \"234234\",\n" +
+                "    \"goodsUnit\": \"地方撒地方\",\n" +
+                "    \"goodsAmt\": \"sdfasdf\"\n" +
+                "}";
+        GoodInfo goodInfo = JSONObject.parseObject(str, GoodInfo.class);
+        Console.show(JSON.toJSONString(goodInfo));*/
+
+        /*String str = "http://liugs.utools.club/pay-dev/pay/rest/receiveIcbcNotify";
+        Console.show(str.substring(str.indexOf("/pay/")));
+        String SLASH = "/";
+        Matcher slashMatcher = Pattern.compile(SLASH).matcher(str);
+        int index = 0;
+        while(slashMatcher.find()) {
+            index++;
+            if(index == 3){
+                break;
+            }
+        }
+        Console.show(slashMatcher.start());
+        Console.show(str.substring(slashMatcher.start()));*/
+
+        /*JSONObject json = new JSONObject();
+        json.put("billDate", "20201224163121");
+        Map<String, String> paramMap = new HashMap<>(16);
+        paramMap.put("MerId", "23423423");
+        json.put("paraMap", paramMap);
+        Console.show(JSON.toJSONString(json));*/
+        TestParam("1234324", "42342342", "42342342");
+
+        List<String> list = new ArrayList<>();
+        Map<String, String> paramMap = new HashMap<>(list.size());
+        Console.show(paramMap);
+    }
+
+    public static void TestParam(Object...arg) {
+
+    }
+
+    public static class GoodInfo {
+        /**商品信息子序号*/
+        private String goodsSubId;
+        /** 商品名称 */
+        private String goodsName;
+        /** 收款人户名 */
+        private String payeeCompanyName;
+        /** 商品数量 */
+        private String goodsNumber;
+        /** 商品单位 */
+        private String goodsUnit;
+        /** 商品金额（单位：分） */
+        private String goodsAmt;
+
+        public String getGoodsSubId() {
+            return goodsSubId;
+        }
+
+        public void setGoodsSubId(String goodsSubId) {
+            this.goodsSubId = goodsSubId;
+        }
+
+        public String getGoodsName() {
+            return goodsName;
+        }
+
+        public void setGoodsName(String goodsName) {
+            this.goodsName = goodsName;
+        }
+
+        public String getPayeeCompanyName() {
+            return payeeCompanyName;
+        }
+
+        public void setPayeeCompanyName(String payeeCompanyName) {
+            this.payeeCompanyName = payeeCompanyName;
+        }
+
+        public String getGoodsNumber() {
+            return goodsNumber;
+        }
+
+        public void setGoodsNumber(String goodsNumber) {
+            this.goodsNumber = goodsNumber;
+        }
+
+        public String getGoodsUnit() {
+            return goodsUnit;
+        }
+
+        public void setGoodsUnit(String goodsUnit) {
+            this.goodsUnit = goodsUnit;
+        }
+
+        public String getGoodsAmt() {
+            return goodsAmt;
+        }
+
+        public void setGoodsAmt(String goodsAmt) {
+            this.goodsAmt = goodsAmt;
+        }
+    }
+
+    /**
+     * 描述 list排序
+     * @author liugs
+     * @date 2020/7/29 10:51:04
+     */
+    private static void dealListSort() {
+        DateTime date = new DateTime();
+        Console.show(date);
+
+        ListDemo demo1 = new ListDemo();
+        demo1.setDateTime(date.toDate());
+        demo1.setIndex(1);
+        ListDemo demo2 = new ListDemo();
+        demo2.setDateTime(date.plusDays(1).toDate());
+        demo2.setIndex(2);
+        ListDemo demo3 = new ListDemo();
+        demo3.setDateTime(date.plusHours(2).toDate());
+        demo3.setIndex(3);
+
+        List<ListDemo> listDemos = new ArrayList<>();
+        listDemos.add(demo1);
+        listDemos.add(demo2);
+        listDemos.add(demo3);
+
+        Console.show("==========before sort=========");
+        for (ListDemo demo : listDemos) {
+            Console.show("index：" + demo.getIndex() + ", dateTime：" + new DateTime(demo.getDateTime()).toString("yyyy年MM月dd日HH时mm分ss秒"));
+        }
+
+        /*Collections.sort(listDemos, new Comparator<ListDemo>() {
+            @Override
+            public int compare(ListDemo o1, ListDemo o2) {
+                if (o1.getDateTime().before(o2.getDateTime())) {
+                    return 1;
+                } else if (o1.getDateTime().equals(o2.getDateTime())) {
+                    return 0;
+                }
+                return -1;
+            }
+        });*/
+
+        Collections.sort(listDemos, (o1, o2) -> {
+            if (o1.getDateTime().before(o2.getDateTime())) {
+                return 1;
+            } else if (o1.getDateTime().equals(o2.getDateTime())) {
+                return 0;
+            }
+            return -1;
+        });
+
+        Console.show("==========after sort=========");
+       for (ListDemo demo : listDemos) {
+           Console.show("index：" + demo.getIndex() + ", dateTime：" + new DateTime(demo.getDateTime()).toString("yyyy年MM月dd日HH时mm分ss秒"));
+       }
+    }
+
+    /**
+     * list 排序demo
+     */
+    private static class ListDemo {
+        public Date dateTime;
+        public Integer index;
+
+        public Date getDateTime() {
+            return dateTime;
+        }
+
+        public void setDateTime(Date dateTime) {
+            this.dateTime = dateTime;
+        }
+
+        public Integer getIndex() {
+            return index;
+        }
+
+        public void setIndex(Integer index) {
+            this.index = index;
+        }
+    }
+
+    /**
+     * 描述 测试异常类
+     * @author liugs
+     * @date 2020/7/16 11:02:49
+     */
+    private static void ToolExceptionTest() {
+        throw new ToolException("1000001", "错误");
+    }
+
+    /**
+     * 描述 测试RSA加密
+     * @author liugs
+     * @date 2020/7/9 9:39:26
+     */
+    private static void enCode() throws Exception {
+        Map<String, String> keyMap = RsaEncodeTool.generateRSAKeys();
+
+        String publicKey = keyMap.get(ToolConstants.EncodeConstants.PUBLIC_KEY);
+        String privateKey = keyMap.get(ToolConstants.EncodeConstants.PRIVATE_KEY);
+        console("publicKey：" + publicKey);
+        console("privateKey：" + privateKey);
+        //获取一个签名Key
+        String signKey = RsaEncodeTool.getRandomStringByLength(32);
+        //待加密对象
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "tester");
+        jsonObject.put("age", "100");
+        jsonObject.put("sex", "man");
+        String resourceStr = jsonObject.toJSONString();
+
+//        String QQStr = "1138208740";
+
+        console("==================公钥加密，私钥解密==================");
+        //公钥加密串
+        String encodeStr = EncodeExecuter.publicEncode(publicKey, resourceStr, signKey);
+        console("encodeStr：" + encodeStr);
+        //私钥解密
+        String decodeStr = EncodeExecuter.privateDecode(privateKey, encodeStr);
+        console("decodeStr：" + decodeStr);
+
+        console("==================私钥加密，公钥解密==================");
+        //私钥加密
+        String encodeStrByPriKey = EncodeExecuter.privateEncode(privateKey, resourceStr, signKey);
+        console("encodeStrByPriKey：" + encodeStrByPriKey);
+        //公钥解密
+        String decodeStrByPubKey = EncodeExecuter.publicDeCode(publicKey, encodeStrByPriKey);
+        console("decodeStrByPubKey：" + decodeStrByPubKey);
+
+        //校验签名
+        console(EncodeExecuter.validateSign(decodeStr, signKey));
+    }
+
+    /**
+     * 描述 金额单位转换，及保留位数
+     * @return void
+     * @author liugs
+     * @date 2020/7/10 9:36:14
+     */
+    private static void dealNum() {
+        BigDecimal yuan = new BigDecimal(1);
+        BigDecimal wan = yuan.divide(new BigDecimal(10000), 2, BigDecimal.ROUND_DOWN);
+        BigDecimal fen = yuan.multiply(new BigDecimal(100)).divide(BigDecimal.ONE, 2, BigDecimal.ROUND_DOWN);
+        console("元：" + yuan);
+        console("万：" + wan);
+        console("分：" +fen);
+    }
+
+    /**
+     * 描述 打印
+     * @param obj
+     * @return void
+     * @author liugs
+     * @date 2020/7/9 10:31:10
+     */
+    private static void console(Object obj) {
+        System.out.println(obj);
+    }
+
+
+    public static void sort() {
+     /*   List<String> strs = new ArrayList<>();
+        char[] chars = {'a', 'b', 'c', 'd'};
+        char[] target = new char[4];
+        for (int i = 0; i < 4; i ++) {
+            char first = chars[i];
+            for (int j = 0; j < 4; j ++) {
+                char second = chars[j];
+                if (first == second) {
+                    continue;
+                }
+                for (int k = 0; k < 4; k ++) {
+                    char third = chars[k];
+                    if (first == third || second == third) {
+                        continue;
+                    }
+                    for (int l = 0; l < 4; l ++) {
+                        char fouth = chars[l];
+                        if (first == fouth || second == fouth || third == fouth) {
+                            continue;
+                        }
+                        target[0]=first;
+                        target[1]=second;
+                        target[2]=third;
+                        target[3]=fouth;
+                        Console.show(new String(target));
+                    }
+                }
+            }
+        }*/
+    }
+
+    private static void listToMap() {
+        //将列表转为Map<key=name, value = Demo>
+        List<Demo> list = new ArrayList<>();
+        Demo demo = new Demo();
+        demo.setName("ceshi");
+        list.add(demo);
+
+        Demo demo2 = new Demo();
+        demo2.setName("ceshi2");
+        list.add(demo2);
+
+        //list 转 map
+        Map<String, Demo> relMap = list.stream().collect(Collectors.toMap(Demo::getName, retRelPo -> retRelPo));
+
+        //遍历map
+        Iterator iterator = relMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Console.show(JSON.toJSONString(iterator.next()));
+        }
+        for (String demo1 : relMap.keySet()) {
+            Console.show(relMap.get(demo1).getName());
+        }
+    }
+
+    private static void listPage() {
+        int pageNo = 2;
+        int pageSize = 1;
+
+        List<Demo> demos = new ArrayList<>();
+        Demo demo1 = new Demo();
+        demo1.setName("demo1");
+        demos.add(demo1);
+
+        Demo demo2 = new Demo();
+        demo2.setName("demo2");
+        demos.add(demo2);
+
+        List<Demo> retList = new ArrayList<>();
+
+        //获取指针 (如果第一页，就从0开始，如果是其他页码，就用 当前码*页面大小)
+        int index = ( pageNo > 1 ? (pageNo - 1) * pageSize : 0);
+
+        for (int i = 0; i < pageSize && i < demos.size() - index; i++) {
+            Demo dataBo = demos.get(index + i);
+            retList.add(dataBo);
+        }
+
+        int total = demos.size() % pageSize == 0 ? demos.size()/pageSize : demos.size()/pageSize +1;
+        int recodeTotal = demos.size();
+
+        Console.show("total:" + total);
+        Console.show("recodeTotal:" + recodeTotal);
+        Console.show("pageNo:" + pageNo);
+        Console.show("pageSize:" + pageSize);
+        Console.show("retList:" + JSON.toJSONString(retList));
+
+
+    }
+
+    private static class Demo {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
---
+++ tool-base/src/main/java/com/liugs/tool/constant/ToolPropertiesKey.java
@@ -0,0 +1,39 @@
+package com.liugs.tool.constant;
+
+import cn.hutool.core.util.ReflectUtil;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @ClassName ToolPropertiesKey
+ * @Description 配置文件key
+ * @Author liugs
+ * @Date 2020/9/8 14:08:41
+ * @Copyright 2020 www.tydic.com Inc. All rights reserved.
+ * 注意 本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
+ */
+public class ToolPropertiesKey {
+
+    /***/
+    public static String TEST_KEY = "testKey";
+
+    public static  Set<String> getKeySet() {
+        Set<String> keySet = new HashSet<>(16);
+        Map<String, Field> map = ReflectUtil.getFieldMap(ToolPropertiesKey.class);
+        Iterator<Map.Entry<String, Field>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Field> entry = iterator.next();
+            Field field = entry.getValue();
+            try {
+                keySet.add((String) field.get(entry.getKey()));
+            } catch (Exception e) {
+                System.out.println("从ToolPropertiesKey中获取配置文件key异常：" + e);
+            }
+        }
+        return keySet;
+    }
+}
